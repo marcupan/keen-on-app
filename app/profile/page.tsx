f21b42e11d3f28ccc561e1a531dbbe070d5791eb
dzhovtobryukh <10772794+marcupan@users.noreply.github.com>
2025-03-01 20:37:04 +0200

    add query provider

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,14 +1,29 @@
 "use client";
 
+import { useQuery } from "@tanstack/react-query";
+
 import { useAuthCheck } from "@/hooks/useAuthCheck";
 
+async function fetchUser() {
+  const res = await fetch("/api/user/profile");
+  if (!res.ok) {
+    throw new Error("Network response was not ok");
+  }
+  return res.json();
+}
+
 export default function ProfilePage() {
+  const { data, error, isLoading } = useQuery(["userProfile"], fetchUser);
+
   useAuthCheck();
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error loading profile.</p>;
 
   return (
     <div>
       <h1 className="text-2xl font-bold mb-4">Your Profile</h1>
-      <p>Show user profile data here.</p>
+      <p>Name - {data.name}</p>
     </div>
   );
 }

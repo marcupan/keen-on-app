3a37c4fc1b5eb7ba0da2ff94f97b3112a2357ee7
dzhovtobryukh <10772794+marcupan@users.noreply.github.com>
2025-05-13 19:09:04 +0300

    update api-client

--- lib/api-client.ts
+++ lib/api-client.ts
@@ -10,7 +10,7 @@ export async function fetchApi<T>(
 	endpoint: string,
 	options: RequestInit = {}
 ): Promise<T | null> {
-	const res = await fetch(`${config.apiBaseUrl}${endpoint}`, {
+	const response = await fetch(`${config.apiBaseUrl}${endpoint}`, {
 		...options,
 		credentials: 'include',
 		headers: {
@@ -19,11 +19,11 @@ export async function fetchApi<T>(
 		},
 	});
 
-	if (!res.ok) {
+	if (!response.ok) {
 		let errors = ['An error occurred'];
 
 		try {
-			const errorData = await res.json();
+			const errorData = await response.json();
 
 			if (errorData && Array.isArray(errorData.errors)) {
 				errors = errorData.errors.map(
@@ -34,15 +34,15 @@ export async function fetchApi<T>(
 				errors = [errorData.message];
 			}
 		} catch {
-			errors = [`HTTP Error: ${res.status} ${res.statusText}`];
+			errors = [`HTTP Error: ${response.status} ${response.statusText}`];
 		}
 
 		throw new ApiError(errors);
 	}
 
-	if (res.status === 204) {
+	if (response.status === 204) {
 		return null;
 	}
 
-	return res.json();
+	return response.json();
 }
